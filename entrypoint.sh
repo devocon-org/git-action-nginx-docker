55711f0d66e5da6a6c3786f599f68804dffaf785
rio-ke <kendanicrio@gmail.com>
2024-12-28 18:08:51 +0530

    name changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
-echo "### Hello world! :rocket:" >> $GITHUB_STEP_SUMMARY
+echo "### Hello KEN! :rocket:" >> $GITHUB_STEP_SUMMARY
 
